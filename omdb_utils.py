f9d2aca711273ef61ffff0834e2dc22a0354f6db
Nikhil3405 <nikhiln432005@gmail.com>
2025-09-06 15:37:32 +0530

    Update omdb_utils.py

--- omdb_utils.py
+++ omdb_utils.py
@@ -4,11 +4,12 @@ def get_movie_details(title,api_key):
     url = f'https://www.omdbapi.com/?t={title}&plot=full&apikey={api_key}'
     res = requests.get(url).json()
     if res.get('Response') == 'True':
-        result = res.get("Plot",'N/A'),res.get('Poster','N/A'),res.get('Director','N/A'),res.get('Actors','N/A')
+        result = res.get("Plot",'N/A'),res.get('Poster','N/A'),res.get('Director','N/A'),res.get('Actors','N/A'),res.get('Year','N/A')
         plot = result[0]
         poster = result[1]
         director = result[2]
         actors = result[3]
-        return plot,poster,director,actors
+        year = result[4]
+        return plot,poster,director,actors,year
     
-    return 'N/A','N/A','N/A','N/A'
+    return 'N/A','N/A','N/A','N/A','N/A'
